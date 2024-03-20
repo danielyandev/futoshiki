20810d7576c97e78a62cd3cdcd86dd7d688a548e
Ruben Danielyan <rub1994.13@gmail.com>
2024-03-20 23:06:29 +0400

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,24 @@
+function App() {
+
+  return (
+    <div className="container py-5">
+      <h1 className="text-center mb-4">Futoshiki Game</h1>
+      <div className="row">
+        <div className="col-md-8">
+          {/* Game Board will go here */}
+        </div>
+        <div className="col-md-4">
+          {/* Control Panel and Instructions */}
+          <div className="mb-3">
+            {/* Control Panel */}
+          </div>
+          <div>
+            {/* Instructions/Info Section */}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
