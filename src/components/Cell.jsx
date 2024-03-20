984df73284267faf3267a513f4698766b48d73a4
Ruben Danielyan <rub1994.13@gmail.com>
2024-03-21 00:53:40 +0400

    Render constraints

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,6 +1,20 @@
 import PropTypes from 'prop-types'
 
 export default function Cell({ cell }) {
+  const renderRightConstraint = () => {
+    if (!cell.constraints.right) return null
+
+    const side = cell.constraints.right === '>' ? 'right' : 'left'
+    return <i className={`fa fa-chevron-${side}`} />
+  }
+
+  const renderBottomConstraint = () => {
+    if (!cell.constraints.bottom) return null
+
+    const side = cell.constraints.bottom === '>' ? 'bottom' : 'up'
+    return <i className={`fa fa-chevron-${side}`} />
+  }
+
   return (
     <div>
       <div className="d-flex">
@@ -8,11 +22,11 @@ export default function Cell({ cell }) {
           {cell.value}
         </div>
         <div className="cell-constraint-right d-flex justify-content-center align-items-center">
-          {'>'}
+          {renderRightConstraint()}
         </div>
       </div>
       <div className="cell-constraint-bottom d-flex justify-content-center align-items-center">
-        {'<'}
+        {renderBottomConstraint()}
       </div>
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './components/App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './main.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
